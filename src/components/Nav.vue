88c50236174e95d2b78b2f9f326fad9eeedebcb3
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-12-16 18:53:25 +0000

    started nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,7 @@
+<template>
+<nav class="w-[100vw] h-[60px] bg-[#1E1E1E]">
+    <p class="mx-auto text-gray-100 text-[30px] font-serif italic pt-[7px] hover:cursor-pointer w-[100px]">
+    Vision</p>
+</nav>
+
+</template>
