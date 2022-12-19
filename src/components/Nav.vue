ffc1b392e9f7943fd734a89290b30c27788c3d55
Mateusz <kroplewskimateusz@gmail.com>
2022-12-19 17:56:25 +0000

    working on nav bar more started on users

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,17 @@
-<template>
-<nav class="w-[100vw] h-[60px] bg-[#1E1E1E]">
-    <p class="mx-auto text-gray-100 text-[30px] font-serif italic pt-[7px] hover:cursor-pointer w-[100px]">
-    Vision</p>
-</nav>
+<script setup>
+    import profilePrompt from '@/components/profilePrompt.vue';
+    import { ref } from 'vue';
 
+    let showPrompt = ref(true);
+</script>
+
+
+<template>
+<nav class="w-[100vw] h-[70px] bg-[#0080FF]">
+        <p class="text-gray-100 text-[30px] font-serif italic pt-[13px] hover:cursor-pointer w-[100px] mx-auto">
+        Vision</p>
+        <img src="@/assets/images/userIcon2.png" alt="" class="w-[50px] h-[50px] mt-[5px] absolute right-10 top-1 hover:cursor-pointer" @click.prevent='showPrompt= !showPrompt'> 
+        <profilePrompt v-if="showPrompt"></profilePrompt>
+    </nav>
 </template>
+
---
+++ src/components/profilePrompt.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+    <div class="w-[300px] h-[150px] bg-[#4fa5fc] rounded-md absolute right-10 mr-[10px] mt-5">
+        <div class="arrow-up absolute right-0 -top-3"></div>
+        <img src="@/assets/images/close.png" alt="" class="w-[20px] float-right mt-[10px] mr-[10px] hover:cursor-pointer">
+        <h1 class="font-bold text-gray-100 text-[20px] text-center mt-[30px]">You are not logged in!</h1>
+        <div class=" w-[80%] mx-auto mt-5">
+            <button class="w-[100%] h-[40px] bg-gray-200 rounded-md font-semibold text-[#222222]">Log in now!</button>
+        </div>
+    </div>
+</template>
+
+<style>
+.arrow-up {
+  width: 0; 
+  height: 0; 
+  border-left: 15px solid transparent;
+  border-right: 15px solid transparent;
+  
+  border-bottom: 15px solid #4fa5fc;
+}
+</style>
