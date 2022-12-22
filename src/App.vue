a408ef0716489c4a09121cf498f9e5b4b0df9aa4
Mateusz <kroplewskimateusz@gmail.com>
2022-12-22 16:14:02 +0000

    started register

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import Nav from '@/components/Nav.vue';
 import Login from '@/views/logInView.vue';
-
+import Register from '@/views/registerView.vue';
 </script>
 
 <template>
     <Nav></Nav>
-    <Login></Login>
+    <!-- <Login></Login> -->
+    <Register></Register>
 </template>
 
 <style scoped>
