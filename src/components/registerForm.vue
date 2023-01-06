c4cc7b58181e5d062f8314db76a30fa92c488dd2
Mateusz <kroplewskimateusz@gmail.com>
2023-01-06 16:25:49 +0000

    register form validation finished

--- src/components/registerForm.vue
+++ src/components/registerForm.vue
@@ -2,10 +2,20 @@
 import {ref} from 'vue';
 let selectedAvatar = ref(2);
 
+const schema = {
+    name: "required|min:3|max:50|alpha_spaces",
+    email: "required|max:50|email",
+    password: "required|min:5",
+    confirmPassword: "required|password_mismatch:@password",
+};
+
+function register(values){
+    console.log(values);
+}
 </script>
 
 <template>
-    <vee-form action="" class="w-[320px] mx-auto mt-10 text-center">
+    <vee-form action="" @submit="register" :validation-schema="schema" class="w-[320px] mx-auto mt-10 text-center">
         <div class="w-[270px] mx-auto mb-[10px]">
             <p class="text-[20px] font-semibold text-gray-100 mb-[15px]">Choose an avatar:</p>
             <ul class="flex space-x-5">
@@ -24,19 +34,21 @@ let selectedAvatar = ref(2);
                 </ul>
         </div>
         <label for="name" class="text-gray-100 font-semibold text-[20px] mr-[10px]">Full Name:</label>
-        <vee-field type="text" name="name" id="" placeholder="John Doe" class="w-[300px] h-[34px] rounded-md pl-[10px] font-semibold text-[#333333] mb-[5px]" />
-
+        <vee-field type="text" name="name" id="" placeholder="John Doe" class="w-[300px] h-[34px] rounded-md pl-[10px] font-semibold text-[#333333] mb-[15px]" />
+        <ErrorMessage name="name" class="text-red-600 block text-left ml-[10px] font-semibold absolute -mt-[15px]"/>
         <label for="email" class="text-gray-100 font-semibold text-[20px] mr-[10px]">Email:</label>
-        <input type="email" name="email" id="" placeholder="Email" class="w-[300px] h-[34px] rounded-md pl-[10px] font-semibold text-[#333333] mb-[5px]">
+        <vee-field type="email" name="email" id="" placeholder="Email" class="w-[300px] h-[34px] rounded-md pl-[10px] font-semibold text-[#333333] mb-[15px]" />
+        <ErrorMessage name="email" class="text-red-600 block text-left ml-[10px] font-semibold absolute -mt-[15px]"/>
         
         <label for="password" class="text-gray-100 font-semibold text-[20px] mr-[10px] ">Password:</label>
-        <input type="password" name="password" id="" placeholder="Password" class="w-[265px] h-[34px] rounded-md pl-[10px] font-semibold text-[#333333] mb-[5px]">
+        <vee-field type="password" name="password" id="" placeholder="Password" class="w-[265px] h-[34px] rounded-md pl-[10px] font-semibold text-[#333333] mb-[15px]" />
+        <ErrorMessage name="password" class="text-red-600 block text-left ml-[30px] font-semibold absolute -mt-[15px]"/>
 
         <label for="conPassword" class="text-gray-100 font-semibold text-[20px] mr-[10px] ">Confirm password:</label>
-        <input type="password" name="conPassword" id="" placeholder="Confirm Password" class="w-[265px] h-[34px] rounded-md pl-[10px] font-semibold text-[#333333] mb-[5px]">
-        <div class="w-[100px] mx-auto mt-10">
-            <button class="w-[100%] h-[30px] rounded-md bg-gray-200 text-[#333333] font-semibold hover:bg-[#333333] hover:text-gray-200">Register</button>
-        </div>
+        <vee-field type="password" name="confirmPassword" id="" placeholder="Confirm Password" class="w-[265px] h-[34px] rounded-md pl-[10px] font-semibold text-[#333333] mb-[15px]" />
+        <ErrorMessage name="confirmPassword" class="text-red-600 block text-left ml-[30px] font-semibold absolute -mt-[15px]"/>
+
+        <button class="w-[100%] h-[40px] rounded-md bg-gray-200 text-[#333333] font-semibold hover:bg-[#333333] hover:text-gray-200 mt-10">Register</button>
     </vee-form>
 
 
