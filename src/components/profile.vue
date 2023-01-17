c5abe96d95ca5aa40488df889a3107e834b0b532
Mateusz <kroplewskimateusz@gmail.com>
2023-01-17 21:38:07 +0000

    worked more on profile component

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -8,12 +8,17 @@ let profileStore = useProfileStore().user;
 <template>
     <div class="w-[700px] h-[400px] bg-blue-500 mx-auto rounded-md mt-16">
         <p class="font-bold text-center underline text-[30px] pt-5 text-gray-200">Profile</p>
-        <div class="w-[350px] mx-auto text-gray-300 font-semi-bold text-[20px]">
-            <img src="@/assets/images/userIcon2.png" alt="" class="w-[50px] h-[50px]"> 
-            <p>Created at: {{profileStore.created_at}}</p>
-            <p>ID: {{profileStore.id}}</p>
-            <p>Name: {{profileStore.FullName}}</p>
-            <p>Email: {{profileStore.Email}}</p>
+        <div class="w-[360px] mx-auto text-gray-300 font-semi-bold text-[20px] flex flex-col space-y-2">
+            <div class="w-[50px] mx-auto">
+                <img src="@/assets/images/userIcon2.png" alt="" class="w-[50px] h-[50px]"> 
+            </div>
+            <p>Created at: <span class="font-bold text-gray-200 pl-[10px]">{{profileStore.created_at}}</span></p>
+            <p>ID: <span class="font-bold text-gray-200 pl-[10px]">{{profileStore.id}}</span></p>
+            <p>Name: <span class="font-bold text-gray-200 pl-[10px]">{{profileStore.FullName}}</span></p>
+            <p>Email: <span class="font-bold text-gray-200">{{profileStore.Email}}</span></p>
+        </div>
+        <div class="w-[200px] mx-auto pt-10">
+            <button class="w-[200px] h-[35px] rounded-md bg-gray-200 hover:bg-gray-300 font-bold">Log Out</button>
         </div>
     </div>
 </template>
