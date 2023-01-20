909572b1f181d49807f7e91f1354ce12152b6020
Mateusz <kroplewskimateusz@gmail.com>
2023-01-20 16:51:49 +0000

    started working on routing

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,12 @@
 <script setup>
     import profilePrompt from '@/components/profilePrompt.vue';
+    import { useProfileStore } from '../stores/profile';
+
     import { ref } from 'vue';
 
     let showPrompt = ref(false);
+
+    let profileStore = useProfileStore();
 </script>
 
 
@@ -10,8 +14,13 @@
 <nav class="w-[100vw] h-[70px] bg-[#0080FF]">
         <p class="text-gray-100 text-[30px] font-serif italic pt-[13px] hover:cursor-pointer w-[100px] mx-auto">
         Vision</p>
-        <img src="@/assets/images/userIcon2.png" alt="" class="w-[50px] h-[50px] mt-[5px] absolute right-10 top-1 hover:cursor-pointer" @click.prevent='showPrompt= !showPrompt'> 
-        <profilePrompt v-if="showPrompt"></profilePrompt>
+        <div v-if="profileStore.user.id != undefiend">
+            <img src="@/assets/images/userIcon2.png" alt="" class="w-[50px] h-[50px] mt-[5px] absolute right-10 top-1 hover:cursor-pointer" @click.prevent='showPrompt= !showPrompt'> 
+            <profilePrompt v-if="showPrompt"></profilePrompt>
+        </div>
+        <div v-else>
+            <button class="w-[100px] h-[35px] rounded-md bg-[#333333] text-gray-200 font-semibold hover:text-[#333333] hover:bg-gray-200 absolute right-10 top-5">Login</button>
+        </div>
     </nav>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/logInView.vue";
+import Register from "@/views/registerView.vue";
+import ProjectsView from "@/views/projectsView.vue";
+import HomeView from "@/views/homeView.vue";
+import ProfileView from "@/views/profileView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: "/", component: HomeView },
+    { path: "/login", component: Login },
+    { path: "/register", component: Register },
+    { path: "/projects", component: ProjectsView },
+    { path: "/profile", component: ProfileView },
+  ],
 });
 
 export default router;
